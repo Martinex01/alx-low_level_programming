b1c4a8e6449c5955437c6892187b0895387930f9
Your Name <you@example.com>
2022-09-09 04:17:53 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
-i
 #include <stdio.h>
 
 /**
- * main-Entry point
+ * main - Entry point
  *
- * Return: Always 0 (Success)
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return (0)
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
-
