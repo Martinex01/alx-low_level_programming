31690b867dcb0a97e5a26979d67c74fed6461b6a
Your Name <you@example.com>
2022-09-10 05:34:09 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,20 @@
-#include <stdio.h>
+/*
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <unistd.h>
 
 /**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *  followed by a new line, to standard error.
  *
- * main-print quote
- *
- * Decscription: print Dora's quote
- *
- * Return: Always 1 (success)
+ *  Return: Always 1.
  */
-
 int main(void)
 {
-white(2, "and that piece of art is useful\* - Dora Korpar, 2015-10-19\n", 59);
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+
 return (1);
 }
 
