99b2dce66ba01876bfd47a7ffd22d71b3765f284
Your Name <you@example.com>
2022-09-12 21:18:58 -0700

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 int n;
-nt l;
+int l;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 l = n % 10;
@@ -29,3 +29,5 @@ printf("Last digit of %d is %d and is less than 6 and not 0\n", n,l);
 }
 return (0);
 }
+
+
